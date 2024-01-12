70c407c8b7f4b631676fb3fe546913a67cdb8a5d
Gaurav Sanjay Modak <132845424+GauravSanMod@users.noreply.github.com>
2024-01-12 15:13:28 +0530

    initial commit

---
+++ main.js
@@ -0,0 +1,6 @@
+var typed = new Typed(".typing",{
+    strings:["Web Developer","Python Developer","Gen AI/ML Engineer","Software Developer"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
